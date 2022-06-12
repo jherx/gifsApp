96283639c200a680289e715a687225479e3045db
Gerson Santos <jhxn07@gmail.com>
2022-06-12 00:17:13 -0500

    Uso del viewchild  y extracion de el value

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -11,13 +11,15 @@ export class BusquedaComponent  {
 // ViewChild  se puede denominar como un querySelector
 //indicas el nombre del elemento y haces referencia 
 //se vuelve una variable en el ts 
-//! indica que sera una negacion 
-  @ViewChild('txtBusqueda') txtBusqueda!:ElementRef
- 
+//(!) indica que sera una negación del un valor null
+  @ViewChild('txtBusqueda') txtBusqueda!:ElementRef<HTMLInputElement>
+ // ElementRef acepta todo tipo de valores , por lo que se le tiene que indicar que valor debe de usar
+
+
   busqueda(){
     const valor = this.txtBusqueda.nativeElement.value;
     console.log('//'+ valor );
-    
+    this.txtBusqueda.nativeElement.value = ''
   }
 
 }
